cf4726be96ceb683bf0ed385c01d34d0e01ccc87
Michelcalex <Michelcalex+github@gmail.com>
2016-10-04 16:50:02 -0400

    fixed jquery scroll selector

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   // Add smooth scrolling to all links
-  $(".navbar-right a").on('click', function(event) {
+  $(".navbar a").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
